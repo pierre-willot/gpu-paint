64ede261394f43d042ef1e038028e54362dbb21c
Pierre Willot <willotpierre@yahoo.fr>
2026-03-14 20:06:38 +0100

    Add deployment workflow

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This ensures your assets (JS, CSS) load correctly from the /gpu-paint/ subfolder
+  base: '/gpu-paint/', 
+})
